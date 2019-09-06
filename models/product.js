a5cde15171c593c2f9dbf0a65621b0268fd7deb7
Nico <Nico@Nicos-MacBook-Pro.local>
2019-09-06 12:59:54 +0700

    adds product routes and fetchAll

--- models/product.js
+++ models/product.js
@@ -19,7 +19,22 @@ class Product {
         console.log(err);
       });
     }
+
+    static fetchAll() {
+      const db = getDb();
+      return db
+        .collection('products')
+        .find()
+        .toArray()
+        .then(products => {
+          console.log(products);
+          return products;
+        })
+        .catch(err => {
+          console.log(err);
+        });
   }
+}
 
 
 module.exports = Product;
