0ddc1c90b24750c4b3661304fcf527b94aec05c9
Nico <Nico@Nicos-MacBook-Pro.local>
2019-09-06 15:40:26 +0700

    fixes deleting product

--- controllers/admin.js
+++ controllers/admin.js
@@ -88,11 +88,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId)
-    .then(product => {
-      return product.destroy();
-    })
-    .then(result => {
+  Product.deleteById(prodId)
+    .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
     })
