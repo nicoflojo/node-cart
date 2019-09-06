9ce5c93138094ed84c4647db59dd1bbf3420161f
Nico <Nico@Nicos-MacBook-Pro.local>
2019-09-06 09:24:05 +0700

    removes hc password

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'Sugarshow1', {
+const sequelize = new Sequelize('node-complete', 'root', 'node-complete', {
   dialect: 'mysql',
   host: 'localhost'
 });
