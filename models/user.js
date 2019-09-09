79d0f2807c7871365a7e4761b0d75348e799d191
Nico <nico@skubos.com>
2019-09-09 18:12:35 +0700

    appends orders to user

--- models/user.js
+++ models/user.js
@@ -75,6 +75,22 @@ class User {
 
   }
 
+  addOrder() {
+    const db = getDb();
+    return db
+      .collection('orders')
+      .insertOne(this.cart)
+      .then(result => {
+        this.cart = {items: []};
+        return db
+        .collection('users')
+        .updateOne(
+          { _id: new ObjectId(this._id)}, 
+          {$set: {cart: { items: [] } } }
+        );
+      });
+  }
+
   static findByPk(userId) {
     const db = getDb();
     return db
