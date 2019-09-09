cef26c6525abe89363a2d42d33004237312e5601
Nico <nico@skubos.com>
2019-09-09 17:54:06 +0700

    adds delete function to cart

--- models/user.js
+++ models/user.js
@@ -40,6 +40,41 @@ class User {
       );
   }
 
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map(i => {
+      return i.productId;
+    });
+    return db
+      .collection('products')
+      .find({ _id: {$in: productIds}})
+      .toArray()
+      .then(products => {
+        return products.map(p => {
+          return {
+            ...p, 
+            quantity: this.cart.items.find(i => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity
+          };
+        });
+    });
+  }
+
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter(item => {
+      return item.productId.toString() !== productId.toString();
+    });
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id)}, 
+        {$set: {cart: {items: updatedCartItems} } }
+      );
+
+  }
+
   static findByPk(userId) {
     const db = getDb();
     return db
