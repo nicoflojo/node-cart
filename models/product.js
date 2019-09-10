02ace539319ccfae59cfba25d9ce1da9cc03db29
Nico <nico@skubos.com>
2019-09-10 12:03:18 +0700

    adds mongoose product schema

--- models/product.js
+++ models/product.js
@@ -1,3 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  }, 
+  price: {
+    type: Number,
+    required: true
+  },
+  desription: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  }
+});
+
+
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
 
