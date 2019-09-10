5c0f36a1d8dc240c35605dad8c2a5f2703588212
Nico <nico@skubos.com>
2019-09-10 12:42:12 +0700

    creates and adds new users

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-/* app.use((req, res, next) => {
-  User.findById('5baa2528563f16379fc8a610')
+app.use((req, res, next) => {
+  User.findById('5d7737850f1d027483bfaf26')
     .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch(err => console.log(err));
-}); */
+}); 
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -37,6 +37,18 @@ mongoose
     'mongodb+srv://nico:Sugarshow1@cluster0-hck0j.mongodb.net/shop?retryWrites=true&w=majority'
   )
   .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Nico',
+          email: 'nico@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch(err => {
