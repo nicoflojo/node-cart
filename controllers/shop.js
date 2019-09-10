f0911797b7f747480f6f65720db051e3df5dad57
Nico <nico@skubos.com>
2019-09-10 14:27:23 +0700

    populates cart data

--- controllers/shop.js
+++ controllers/shop.js
@@ -44,10 +44,13 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      console.log(user.cart.items);
+      const products = user.cart.items;
       res.render('shop/cart', {
-        path: '/cart',
+        path: '/cart', 
         pageTitle: 'Your Cart',
         products: products
       });
