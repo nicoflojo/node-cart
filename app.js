130baae64bf5a47cd146829a02a64980b3aa84e1
Nico <nico@skubos.com>
2019-09-12 15:21:14 +0700

    adds sessions to users

--- app.js
+++ app.js
@@ -29,15 +29,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store}));
 
-app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-});
-
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,20 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-  /* const isLoggedIn = req
-    .get('Cookie')
-    .split(';')[1]
-    .trim()
-    .split('=')[1]; */
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedIn
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
-  res.redirect('/');
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+    
 };
