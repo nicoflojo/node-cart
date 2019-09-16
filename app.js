634107c507648e7a0c3656695f34b6949cc14dd7
Nico <nico@skubos.com>
2019-09-16 09:59:37 +0700

    fixes syntax error

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
-const keys = require('./keys')l
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
